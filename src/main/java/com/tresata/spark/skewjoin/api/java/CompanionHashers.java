78d5e256c73529225c9ee442a3a5f0ce6b2ef88c
Howard Wu <howardwu@berkeley.edu>
2017-07-16 16:34:21 -0700

    Adds support for Long type in java api

--- src/main/java/com/tresata/spark/skewjoin/api/java/CompanionHashers.java
+++ src/main/java/com/tresata/spark/skewjoin/api/java/CompanionHashers.java
@@ -2,6 +2,18 @@ package com.tresata.spark.skewjoin.api.java;
 
 public class CompanionHashers {
 
+  public static class CMSHasherLong extends CMSHasherWrapper<Long> {
+    @Override
+    public int hash(int a, int b, int width, Long x) {
+      long unModded = (x * a) + b;
+      // Apparently a super fast way of computing x mod 2^p-1
+      // See page 149 of http://www.cs.princeton.edu/courses/archive/fall09/cos521/Handouts/universalclasses.pdf
+      // after Proposition 7.
+      long modded = (unModded + (unModded >> 32)) & Integer.MAX_VALUE;
+      return ((int) modded) % width;
+    }
+  }
+
   public static class CMSHasherInteger extends CMSHasherWrapper<Integer> {
     @Override
     public int hash(int a, int b, int width, Integer x) {
--- src/test/scala/com/tresata/spark/skewjoin/api/java/SkewJoinOperationsSuite.java
+++ src/test/scala/com/tresata/spark/skewjoin/api/java/SkewJoinOperationsSuite.java
@@ -6,6 +6,7 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Ordering;
 import com.tresata.spark.skewjoin.SparkSuite$;
 import com.tresata.spark.skewjoin.api.java.CompanionHashers.CMSHasherInteger;
+import com.tresata.spark.skewjoin.api.java.CompanionHashers.CMSHasherLong;
 import java.io.Serializable;
 import java.util.List;
 import org.apache.spark.api.java.JavaPairRDD;
@@ -23,8 +24,32 @@ public class SkewJoinOperationsSuite implements Serializable {
     return new Tuple2<X, Y>(x, y);
   }
 
+  @SuppressWarnings("unchecked")
   @Test
-  public void testSkewJoinOperation() {
+  public void testSkewJoinOperation_Long() {
+    List<Tuple2<Long, Long>> pairs1 = Lists.newArrayList(tuple2(1L, 1L), tuple2(1L, 1L), tuple2(2L, 1L), tuple2(3L, 1L), tuple2(4L, 1L));
+    List<Tuple2<Long, Long>> pairs2 = Lists.newArrayList(tuple2(1L, 2L), tuple2(1L, 2L), tuple2(6L, 2L), tuple2(4L, 2L), tuple2(5L, 2L));
+
+    JavaPairRDD<Long, Long> rdd1 = jsc().parallelizePairs(pairs1);
+    JavaPairRDD<Long, Long> rdd2 = jsc().parallelizePairs(pairs2);
+
+    SkewJoinOperations<Long, Long> sjRDD1 = new SkewJoinOperations<>(rdd1, Ordering.natural(), new CMSHasherLong());
+    SkewJoinOperations<Long, Long> sjRDD2 = new SkewJoinOperations<>(rdd2, Ordering.natural(), new CMSHasherLong());
+
+    List<Tuple2<Long, Tuple2<Long, Long>>> result = sjRDD1.skewJoin(sjRDD2).sortByKey().collect();
+    List<Tuple2<Long, Tuple2<Long, Long>>> answer = Lists.newArrayList(tuple2(1L, tuple2(1L, 2L)), tuple2(1L, tuple2(1L, 2L)), tuple2(1L, tuple2(1L, 2L)), tuple2(1L, tuple2(1L, 2L)), tuple2(4L, tuple2(1L, 2L)));
+
+    for (int i = 0; i < answer.size(); i++) {
+      System.out.println(result.get(i) + " == " + answer.get(i));
+      assertTrue(result.get(i)._1.equals(answer.get(i)._1));
+      assertTrue(result.get(i)._2._1.equals(answer.get(i)._2._1));
+      assertTrue(result.get(i)._2._2.equals(answer.get(i)._2._2));
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void testSkewJoinOperation_Integer() {
     List<Tuple2<Integer, Integer>> pairs1 = Lists.newArrayList(tuple2(1, 1), tuple2(1, 1), tuple2(2, 1), tuple2(3, 1), tuple2(4, 1));
     List<Tuple2<Integer, Integer>> pairs2 = Lists.newArrayList(tuple2(1, 2), tuple2(1, 2), tuple2(6, 2), tuple2(4, 2), tuple2(5, 2));
 
